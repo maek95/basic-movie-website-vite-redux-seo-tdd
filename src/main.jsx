61dfcaa0eced07056c26519342984b9756e25a93
maek95 <mekstrom01@gmail.com>
2024-10-10 11:23:53 +0200

    initialize google analytics as well

--- src/main.jsx
+++ src/main.jsx
@@ -6,17 +6,31 @@ import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './redux/store.jsx'
 import TagManager from 'react-gtm-module';
+import ReactGA from 'react-ga4' 
 
 
 // https://www.npmjs.com/package/react-gtm-module
 // npm install react-gtm-module --save
 // npm install --save-dev @types/react-gtm-module 
 // had to install the types because i dont know?
-const tagManagerArgs = {
-  gtmId: import.meta.env.VITE_GTM_ID, // client-side so we use VITEs .env import
+// Check if environment variables are set
+const gtmId = import.meta.env.VITE_GTM_ID;
+const gaId = import.meta.env.VITE_GA_ID;
+
+if (gtmId) {
+  // Initialize Google Tag Manager
+  const tagManagerArgs = { gtmId };
+  TagManager.initialize(tagManagerArgs);
+} else {
+  console.warn('Google Tag Manager ID (VITE_GTM_ID) is not set in environment variables.');
 }
 
-TagManager.initialize(tagManagerArgs);
+if (gaId) {
+  // Initialize Google Analytics 4
+  ReactGA.initialize(gaId);
+} else {
+  console.warn('Google Analytics ID (VITE_GA_ID) is not set in environment variables.');
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
