98fffe3ac631b9f17d753f3172c691b458c27004
maek95 <mekstrom01@gmail.com>
2024-10-10 11:02:53 +0200

    initialize google tag manager, no events created yet

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,18 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './redux/store.jsx'
+import TagManager from 'react-gtm-module';
+
+
+// https://www.npmjs.com/package/react-gtm-module
+// npm install react-gtm-module --save
+// npm install --save-dev @types/react-gtm-module 
+// had to install the types because i dont know?
+const tagManagerArgs = {
+  gtmId: import.meta.env.VITE_GTM_ID, // client-side so we use VITEs .env import
+}
+
+TagManager.initialize(tagManagerArgs);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
